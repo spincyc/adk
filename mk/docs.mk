75cea680ee1fa6f2035104b3dff847b67e50227d
ADK Project <71109625+spincyc@users.noreply.github.com>
2026-07-27 10:14:41 -0500

    lessons: teach piezo timer ownership

--- mk/docs.mk
+++ mk/docs.mk
@@ -11,6 +11,8 @@ doc/lessons/003.pdf: docs/lessons/003/main.tex \
 		docs/lessons/assets/003-reaction-timer-pencil.png
 doc/lessons/004.pdf: docs/lessons/004/main.tex \
 		docs/lessons/assets/004-pwm-rgb-pencil.png
+doc/lessons/005.pdf: docs/lessons/005/main.tex \
+		docs/lessons/assets/005-piezo-pencil.png
 
 $(LESSON_PDFS): | $(BUILD_MARKER)
 	mkdir -p "$(BUILD_DIR)/lessons/$(basename $(notdir $@))" doc/lessons
