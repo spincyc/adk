d637b2fbcf6673dc98224990762b4b678de5aef4
ADK Project <71109625+spincyc@users.noreply.github.com>
2026-07-28 07:31:57 -0500

    Add Lesson 034 Mega example
    Task-ID: f5da4b9a-d53a-4159-9fda-e9a940c84e98

--- mk/config.mk
+++ mk/config.mk
@@ -6,7 +6,7 @@ BUILD_DIR   ?= build
 BUILD_MARKER := $(BUILD_DIR)/.adk-build
 BOARD_FQBN  ?= arduino:avr:mega
 ARDUINO_AVR_CORE ?= arduino:avr@1.8.8
-LESSONS     := 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033
+LESSONS     := 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034
 EXAMPLES    := \
 	Lesson001DigitalOutput \
 	Lesson002DigitalInput \
@@ -40,7 +40,8 @@ EXAMPLES    := \
 	Lesson030InertShowSimulator \
 	Lesson031AnalogJoystick \
 	Lesson032QuadratureEncoder \
-	Lesson033CalibrationConsole
+	Lesson033CalibrationConsole \
+	Lesson034MagneticObservation
 PORT        ?=
 BAUD        ?= 115200
 SERIAL_LOG  ?= $(BUILD_DIR)/serial/monitor.log
