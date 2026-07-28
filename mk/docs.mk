209c07419e04871721e80ff5cdc8dd58a60ddda0
ADK Project <71109625+spincyc@users.noreply.github.com>
2026-07-28 05:34:29 -0500

    Make Lessons 019 and 020 fast and self-validating
    Task-ID: ba1ed653-0cb2-4275-ae36-16c88591ce35
    Task-ID: 778a0a46-7969-4a38-b206-8ed54ca72d03
    Task-ID: 764b2584-21f9-40c2-a92d-e1714c45d8e2

--- mk/docs.mk
+++ mk/docs.mk
@@ -43,9 +43,11 @@ doc/lessons/017.pdf: docs/lessons/017/main.tex \
 doc/lessons/018.pdf: docs/lessons/018/main.tex \
 		docs/lessons/assets/018-access-trainer-pencil.png
 doc/lessons/019.pdf: docs/lessons/019/main.tex \
-		docs/lessons/assets/019-ultrasonic-range-pencil.png
+		docs/lessons/assets/019-ultrasonic-range-pencil.png \
+		docs/lessons/assets/019-ultrasonic-range-pencil.tex
 doc/lessons/020.pdf: docs/lessons/020/main.tex \
-		docs/lessons/assets/020-motor-intent-pencil.png
+		docs/lessons/assets/020-motor-intent-layout.tex \
+		docs/lessons/assets/020-motor-intent-layout.png
 doc/lessons/021.pdf: docs/lessons/021/main.tex \
 		docs/lessons/assets/021-rover-pencil.png
 doc/lessons/022.pdf: docs/lessons/022/main.tex \
